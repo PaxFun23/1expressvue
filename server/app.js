2eca41b7d992f2e6f6dd64124baef7de5d5f6d41
= <stefan.kresoja23@gmail.com>
2017-09-11 11:02:19 +0200

    Express server instalation

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.port || 8081;
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.json({
+        msg:"HI"
+    });
+});
+
+app.listen(port, ()=>{
+    console.log('Server running on port',port);
+});
